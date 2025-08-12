756108629bc681a888b26bb343391660d6b4dc85
viniciuspadovam <padovam3373@gmail.com>
2025-08-12 12:30:51 -0300

    feat: loading

--- frontend/src/app/features/loading/loading.component.ts
+++ frontend/src/app/features/loading/loading.component.ts
@@ -1,18 +1,34 @@
 import { Component } from '@angular/core';
-import { RoomService } from '../../core/service/room.service';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Router, RouterModule } from '@angular/router';
+import { RoomService } from '../../core/service/room.service';
 
 @Component({
   selector: 'app-loading',
-  imports: [RouterModule],
+  imports: [RouterModule, MatProgressSpinnerModule],
   templateUrl: './loading.component.html',
   styleUrl: './loading.component.css'
 })
 export class LoadingComponent {
 
+  private loadingLabels: string[] = [
+    'Verificando sala',
+    'Aguardando jogadores',
+    'Quase lá...'
+  ];
+  private currentLabelIndex = 0;
+
   constructor(private roomService: RoomService, private router: Router) {}
 
+  get loadingLabel(): string {
+    return this.loadingLabels[this.currentLabelIndex];
+  }
+
   ngOnInit() {
+    setInterval(() => {
+      this.currentLabelIndex = (this.currentLabelIndex + 1) % this.loadingLabels.length;
+    }, 1500);
+
     this.roomService.hasRoom().subscribe(hasRoom => {
       this.router.navigate(['/join'], { queryParams: { hasRoom } });
     });
